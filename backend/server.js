dd4673007f34315f8ab49909a71c32d9b8ca87c7
usmanahmedgoray <gorayausman061@gmail.com>
2024-11-06 23:09:15 +0500

    update cors error

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ require("dotenv").config();
 const app = express();
 require("./db/connection");
 app.use(cors({
-  origin: 'https://community-app-mocha.vercel.app',
+  origin: 'https://community-app-mocha.vercel.app', // Update to the front-end's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 }));
 
 app.use(express.json());
