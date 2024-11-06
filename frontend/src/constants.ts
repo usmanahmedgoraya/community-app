69dac1bf553c898296004ccb24518fd20ae266ae
usmanahmedgoray <gorayausman061@gmail.com>
2024-11-06 22:45:22 +0500

    backend url update

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,3 @@
 // export const backendUrl : string = "https://community-project-backend.onrender.com" 
-export const backendUrl : string = "http://localhost:9000" 
+export const backendUrl : string = "https://community-app-project-backend.vercel.app/" 
+// export const backendUrl : string = "http://localhost:9000" 
